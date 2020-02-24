44b435f6074e024b051a6432206ac94c62698651
ktranfullerton2000 <42751361+ktranfullerton2000@users.noreply.github.com>
2020-02-23 22:24:07 -0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,141 @@
+//
+//  main.c
+//  rpn
+//
+//  Created by William McCarthy on 192//20.
+//  Copyright © 2020 William McCarthy. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+
+#define MAXOP   100    /* max size of operand/operator */
+#define NUMBER '0'     /* signal that a number was found */
+#define MAXVAL  100
+#define NAME 'n'
+#define VARMAX 26
+
+size_t sp = 0;   // aka unsigned long -- printf using %zu
+double val[MAXVAL];   // stack of values
+
+char buf[BUFSIZ];
+size_t bufp = 0;
+
+int getch_(void) { return (bufp > 0) ? buf[--bufp] : getchar(); }
+
+void ungetch_(int c) {
+  if (bufp >= BUFSIZ) { fprintf(stderr, "ungetch: too many characters\n");  return; }
+  buf[bufp++] = c;
+}
+
+int getop(char* s) {
+  int i, c;
+  while ((s[0] = c = getch_()) == ' ' || c == '\t') { }  // skip whitespace
+  s[1] = '\0';
+  
+  if (!isdigit(c) && c != '.' && c!='-') { return c; }  // if not a digit, return
+
+  i = 0;
+  
+  if(c=='-'){
+    if(isdigit(c=getch_()) || c == '.'){
+      s[++i]=c;}
+    else{
+      if(c!=EOF)
+      ungetch_(c);
+      return '-';}}
+
+  if (isdigit(c)) {  // get digits before '.'
+    while (isdigit(s[++i] = c = getch_())) { }
+  }
+  if (c == '.') {    // get digits after decimal (if any)
+    while (isdigit(s[++i] = c = getch_())) { }
+  }
+  s[i] = '\0';
+  if (c != EOF) { ungetch_(c); }
+  
+  return NUMBER;      // return type is NUMBER, number stored in s
+}
+
+double pop(void) {
+  if (sp == 0) { fprintf(stderr, "stack underflow\n");  return 0.0; }
+  return val[--sp];
+}
+
+void push(double f) {
+  if (sp == MAXVAL) { fprintf(stderr, "stack overflow -- can't push %g\n", f);  return; }
+  val[sp++] = f;
+}
+
+void clear(void){
+  sp = 0;
+}
+
+void rpn(void) {
+  int type,v;
+  double op1,op2,mr;//mr is the most recently printed value
+  char s[BUFSIZ];
+  double variable[VARMAX];
+
+  while ((type = getop(s)) != EOF) {
+    switch(type) {
+      case '\n':    
+        mr = pop();
+        printf("\t%.8g\n", mr);  
+        break;
+      case NUMBER:  push(atof(s));              break;
+      case '+':     push(pop() + pop());        break;
+      case '*':     push(pop() * pop());        break;
+      case '-':     push(-(pop() - pop()));     break;
+      case '/':
+        if ((op2 = pop()) == 0.0) { fprintf(stderr, "divide by zero\n");  break; }
+        push(pop() / op2);
+        break;
+      case '%':
+        if ((op2 = pop()) == 0.0) { fprintf(stderr, "module by zero\n"); break; }
+        push(fmod(pop(),op2));;
+        break;
+      case 'c':     clear();                    break; //clear stack
+      case 'p': //print the top elements of the stack without popping
+        op2 = pop();
+        printf("\t%.8g\n", op2);
+        push(op2);
+        break;
+      case 'd': //duplicate
+        op2 = pop();
+        push(op2);
+        push(op2);
+        break;
+      case 's': //swap
+        op1 = pop();
+        op2 = pop();
+        push(op1);
+        push(op2);
+        break;
+      case '$': push(sin(pop()));               break;//sine function
+      case '^': //power
+        op2 = pop();
+        push(pow(pop(), op2));
+        break;
+      case 'e': push(exp(pop()));               break;//exponent of e
+      case '=':
+        pop();
+        if(v>= 'A' && v <= 'Z') variable[v-'A'] = pop(); break;
+        printf("invalid variable name\n"); break;
+      default:      
+        if(type>='A' && type<='Z') push(variable[type-'A']);
+        else if(type == 'v') push(mr);
+        else fprintf(stderr, "unknown command %s\n", s);  break;
+    }
+    v = type;
+  }
+}
+
+int main(int argc, const char * argv[]) {
+  rpn();
+
+  return 0;
+}
