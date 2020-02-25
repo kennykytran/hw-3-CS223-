c7e424e851046ae02c9790d2cd5303fd32faff4b
ktranfullerton2000 <42751361+ktranfullerton2000@users.noreply.github.com>
2020-02-24 22:15:01 -0800

    Update main.c

--- main.c
+++ main.c
@@ -12,11 +12,12 @@
 #include <math.h>
 #include <string.h>
 
-#define MAXOP   100    /* max size of operand/operator */
-#define NUMBER '0'     /* signal that a number was found */
-#define MAXVAL  100
-#define MATH 'n'
-#define VARMAX 26
+#define MAXOP    100    /* max size of operand/operator */
+#define NUMBER   '0'    /* signal that a number was found */
+#define MAXVAL   100
+#define MATH     'n'
+#define VARIABLE 'u'
+#define VARMAX    26
 
 size_t sp = 0;   // aka unsigned long -- printf using %zu
 double val[MAXVAL];   // stack of values
@@ -36,23 +37,33 @@ int getop(char* s) {
   while ((s[0] = c = getch_()) == ' ' || c == '\t') { }  // skip whitespace
   s[1] = '\0';
   
-  if (isalpha(c)){
+  /*EXERCISE 4-6*/
+  if (c == '=' || c == '?'){
     i = 0;
-    while (isalpha(s[++i] = c = getch_())){}
+    while (isalpha(s[++i] = c = getch_()));
     s[i] = '\0';
-    return MATH; }
+    return VARIABLE;
+  }
 
+  if(c >= 'a' && c <= 'z'){
+    i = 0;
+    while (isalpha(s[++i] = c = getch_()));
+    s[i] = '\0';
+    return MATH;
+  }
+  
   if (!isdigit(c) && c != '.' && c!='-') { return c; }  // if not a digit, return
 
   i = 0;
-  
+  /*EXERCISE 4-3*/
   if(c=='-'){
     if(isdigit(c=getch_()) || c == '.'){
       s[++i]=c;}
     else{
       if(c!=EOF)
       ungetch_(c);
-      return '-';}}
+      return '-';}
+  }
 
   if (isdigit(c)) {  // get digits before '.'
     while (isdigit(s[++i] = c = getch_())) { }
@@ -60,6 +71,7 @@ int getop(char* s) {
   if (c == '.') {    // get digits after decimal (if any)
     while (isdigit(s[++i] = c = getch_())) { }
   }
+
   s[i] = '\0';
   if (c != EOF) { ungetch_(c); }
   
@@ -83,77 +95,67 @@ void clear(void){
 void math(char s[])//edit later
 {
   double op2, result;
-  if(strcmp(s,"sin")==0) result = sin(pop());
-  else if(strcmp(s,"cos")==0) result = cos(pop());
-  else if(strcmp(s,"exp")==0) result = exp(pop());
-  else if(strcmp(s,"pow")==0){
+  result = 0;
+
+  if(strcmp(s, "sin") == 0){ result = sin(pop());}
+  else if(strcmp(s, "cos") == 0){ result = cos(pop());}
+  else if(strcmp(s, "exp") == 0){ result = exp(pop());}
+  else if(strcmp(s, "pow") == 0){
       op2 = pop();
       result = pow(pop(),op2);
-      }
-  else{
-      printf("%s not valid math operator\n",s);
+      push(result);}
+  else{printf("%s is a invalid math operator\n",s);}
+  
+  push(result);
+  printf("\t%.8f\n", result);
+}
+
+void var(int vari, double var[]){
+  pop();
+  if(vari >= 'A' && vari <= 'Z') {
+  var[vari -'A'] = pop();
   }
-    push(result);
-    printf("%.3f\n", result);
 }
 
 void rpn(void) {
-  int type,v;
-  double op1,op2,mr;//mr is the most recently printed value
+  int type,vari;
+  double op1,op2, v;
   char s[BUFSIZ];
   double variable[VARMAX];
-
+  
   while ((type = getop(s)) != EOF) {
     switch(type) {
-      case '\n':    
-        mr = pop();
-        printf("\t%.8g\n", mr);  
-        break;
-      case NUMBER:  push(atof(s));              break;
-      case MATH:    math(s);                    break;
-      case '+':     push(pop() + pop());        break;
-      case '*':     push(pop() * pop());        break;
-      case '-':     push(-(pop() - pop()));     break;
+      case '\n': v = pop(); printf("\t%.8g\n", v);  break;
+      case NUMBER: push(atof(s));                   break;
+      /*EXERCISE 4-6*/
+      case VARIABLE: var(vari,variable);            break;
+      /*EXERCISE 4-5*/
+      case MATH: math(s);                           break;
+      /*EXERCISE 4-4*/
+      case '!': printf("\t%.8g\n", (op2=pop())); push(op2);     break; //print
+      case '#': op2 = pop(); push(op2); push(op2);              break;//duplicate
+      case '$': op2 = pop(); op1 = pop(); push(op2); push(op1); break; //swap
+      case '~': clear();                    break; //clear stack
+      case '+': push(pop() + pop());        break;
+      case '*': push(pop() * pop());        break;
+      case '-': push(-(pop() - pop()));     break;
       case '/':
-        if ((op2 = pop()) == 0.0) { fprintf(stderr, "divide by zero\n");  break; }
+        if ((op2=pop())== 0.0) { fprintf(stderr, "divide by zero\n");  break; }
         push(pop() / op2);
         break;
+      /*EXERCISE 4-3*/
       case '%':
-        if ((op2 = pop()) == 0.0) { fprintf(stderr, "module by zero\n"); break; }
-        push(fmod(pop(),op2));;
+        if ((op2=pop())== 0.0) { fprintf(stderr, "module by zero\n"); break; }
+        push(fmod(pop(),op2)); 
         break;
-      case 'c':     clear();                    break; //clear stack
-      case 'p': //print the top elements of the stack without popping
-        op2 = pop();
-        printf("\t%.8g\n", op2);
-        push(op2);
-        break;
-      case 'd': //duplicate
-        op2 = pop();
-        push(op2);
-        push(op2);
-        break;
-      case 's': //swap
-        op1 = pop();
-        op2 = pop();
-        push(op1);
-        push(op2);
-        break;
-      case '^': //power
-        op2 = pop();
-        push(pow(pop(), op2));
-        break;
-      case 'e': push(exp(pop()));               break;//exponent of e
-      case '=':
-        pop();
-        if(v>= 'A' && v <= 'Z') variable[v-'A'] = pop(); break;
-        fprintf(stderr, "invalid variable name\n"); break;
+
+      /*EXERCISE 4-6*/
+      case '@': push(v); break; //output most recently printed value      
       default:      
-        if(type>='A' && type<='Z') push(variable[type-'A']);
-        else if(type == 'v') push(mr);
-        else fprintf(stderr, "unknown command %s\n", s);  break;
+      if(type >= 'A' && type <= 'Z') {push(variable[type- 'A']); break;}
+      fprintf(stderr, "unknown command %s\n", s);  break;
     }
-    v = type;
+    vari = type;
   }
 }
 
